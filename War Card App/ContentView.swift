c29134496782bb2270369a30faacc77e473c9074
Joe Seigler <Joe.seigler06@gmail.com>
2024-02-23 09:33:30 -0600

    Initial Commit

---
+++ War Card App/ContentView.swift
@@ -0,0 +1,84 @@
+//
+//  ContentView.swift
+//  War Card App
+//
+//  Created by 64000270 on 2/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var playerCard = "card7"
+    @State var cpuCard = "card13"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    var body: some View {
+        ZStack{
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("Computer")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+        
+    }
+    
+    func deal(){
+        //Randomize the player card
+        playerCard = "card" + String(Int.random(in: 2...14))
+        //Randomize the cpu card
+        cpuCard = "card" + String(Int.random(in: 2...14))
+        //Update the score
+        
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ War Card App/War_Card_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_AppApp.swift
+//  War Card App
+//
+//  Created by 64000270 on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
